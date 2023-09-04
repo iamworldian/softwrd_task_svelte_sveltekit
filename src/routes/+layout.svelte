16766320f1cafaba5129f3b31aa65045f8af67f4
Mohammed Ashiqur Rahman Baig <ashiqur1107@gmail.com>
2023-09-04 22:52:05 +0600

    home page message added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 
 <nav class=" bg-amber-50">
         <div class="flex ">
+            <div class="my-auto p-4 hover:bg-yellow-300 rounded-lg"><a href="/">Home</a></div>
             <div class="my-auto p-4 hover:bg-yellow-300 rounded-lg"><a href="/layout">Layout</a></div>
             <div class="my-auto p-4 hover:bg-yellow-300 rounded-lg"><a href="/chart">Chart</a></div>
             <div class="my-auto p-4 hover:bg-yellow-300 rounded-lg"><a href="/map">Map</a></div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+
+
+<div class="cont bg-yellow-100 rounded-lg">
+    <div class="msg bg-amber-300 p-5">
+        <h1 class="text-7xl underline bg-amber-500 p-5 rounded-xl"> Softwrd Task </h1>
+        <p class="text-5xl p-5">
+            The app is working fine as instructed except the map load time is little long beacause 
+            its need to pull the geojson data from github which is <span class="text-yellow-600">25 MB</span> of size.
+        </p>
+        <br>
+
+        <p class="text-5xl bg-amber-200 p-2 rounded-xl">
+            Check the <span><a href="https://github.com/iamworldian/softwrd_task_svelte_sveltekit" class="text-blue-500 underline ">Github repo</a></span>
+            for source codes
+        </p>
+        <br>
+        <p class="text-3xl"> Candidate : Md.Ashiqur Rahman <span><a href="#">marb.jobs@gmail.com</a></span></p>
+    </div>
+</div>
+
+<style>
+    .cont{
+        width: 100vw;
+        height: 95vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .msg{
+        width: 60vw;
+    }
+</style>
